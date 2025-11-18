0f3de49751ed8c2772a475951348404538b5c6a2
phuocn0302 <phuocnguyen230302@gmail.com>
2025-11-18 21:53:51 +0700

    feat(wezterm): add fallback font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,20 +2,21 @@ local wezterm = require("wezterm")
 local success, wal_colors = pcall(dofile, os.getenv("HOME") .. "/.cache/wal/colors-wez.lua")
 
 return {
-  font = wezterm.font("MartianMono NF", {
-    weight = "Regular",
+	font = wezterm.font_with_fallback({
+    { family = "MartianMono NF", weight = "Regular"},
+    { family = "M PLUS 1", weight = "Bold"},
   }),
 
-  colors = success and wal_colors or nil,
+	colors = success and wal_colors or nil,
 
-  font_size = 11,
-  window_close_confirmation = "NeverPrompt",
-  enable_tab_bar = false,
+	font_size = 11,
+	window_close_confirmation = "NeverPrompt",
+	enable_tab_bar = false,
 
-  window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 20,
-  },
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
 }
